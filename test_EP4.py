42cb064541570499deb1a1e90ddb2891c837f421
Augusto Lima <augustolimajardim@usp.br>
2021-12-13 17:02:39 -0300

    Resolvido problema do fixture

--- test_EP4.py
+++ test_EP4.py
@@ -2,41 +2,31 @@
 import pytest
 from EP4 import *
 
-'''
+
 @pytest.fixture
 def tabuleiroVazio():
-    t = microTabuleiro()
+    t = microTabuleiro(0,0)
     return t
-'''
 
 class TestaMicroTabuleiro: 
     # Testa se cada posição do tabuleiro foi iniciada vazia
-    def test_tabuleiro_vazio_pos00(self):
-        t = microTabuleiro()
-        assert t.configuracaoDoTabuleiro[0][0] == " "
-    def test_tabuleiro_vazio_pos01(self):
-        t = microTabuleiro()
-        assert t.configuracaoDoTabuleiro[0][1] == " "
-    def test_tabuleiro_vazio_pos02(self):
-        t = microTabuleiro()
-        assert t.configuracaoDoTabuleiro[0][2] == " "
-    def test_tabuleiro_vazio_pos10(self):
-        t = microTabuleiro()
-        assert t.configuracaoDoTabuleiro[1][0] == " "
-    def test_tabuleiro_vazio_pos11(self):
-        t = microTabuleiro()
-        assert t.configuracaoDoTabuleiro[1][1] == " "
-    def test_tabuleiro_vazio_pos12(self):
-        t = microTabuleiro()
-        assert t.configuracaoDoTabuleiro[1][2] == " "
-    def test_tabuleiro_vazio_pos20(self):
-        t = microTabuleiro()
-        assert t.configuracaoDoTabuleiro[2][0] == " "
-    def test_tabuleiro_vazio_pos21(self):
-        t = microTabuleiro()
-        assert t.configuracaoDoTabuleiro[2][1] == " "
-    def test_tabuleiro_vazio_pos22(self):
-        t = microTabuleiro()
-        assert t.configuracaoDoTabuleiro[2][2] == " "
+    def test_tabuleiro_vazio_pos00(self, tabuleiroVazio):
+        assert tabuleiroVazio.configuracaoDoTabuleiro[0][0] == " "
+    def test_tabuleiro_vazio_pos01(self, tabuleiroVazio):
+        assert tabuleiroVazio.configuracaoDoTabuleiro[0][1] == " "
+    def test_tabuleiro_vazio_pos02(self, tabuleiroVazio):
+        assert tabuleiroVazio.configuracaoDoTabuleiro[0][2] == " "
+    def test_tabuleiro_vazio_pos10(self, tabuleiroVazio):
+        assert tabuleiroVazio.configuracaoDoTabuleiro[1][0] == " "
+    def test_tabuleiro_vazio_pos11(self, tabuleiroVazio):
+        assert tabuleiroVazio.configuracaoDoTabuleiro[1][1] == " "
+    def test_tabuleiro_vazio_pos12(self, tabuleiroVazio):
+        assert tabuleiroVazio.configuracaoDoTabuleiro[1][2] == " "
+    def test_tabuleiro_vazio_pos20(self, tabuleiroVazio):
+        assert tabuleiroVazio.configuracaoDoTabuleiro[2][0] == " "
+    def test_tabuleiro_vazio_pos21(self, tabuleiroVazio):
+        assert tabuleiroVazio.configuracaoDoTabuleiro[2][1] == " "
+    def test_tabuleiro_vazio_pos22(self, tabuleiroVazio):
+        assert tabuleiroVazio.configuracaoDoTabuleiro[2][2] == " "
     
         
