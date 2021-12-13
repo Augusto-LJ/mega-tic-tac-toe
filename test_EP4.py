a278c23a99c54e5a8ecbdeb072c83828e218fdd8
Augusto Lima <augustolimajardim@usp.br>
2021-12-13 11:54:09 -0300

    Testes para um tabuleiro vazio

--- test_EP4.py
+++ test_EP4.py
@@ -1,1 +1,35 @@
 # -*- coding: utf-8 -*-
+import pytest
+from EP4 import *
+
+class TestaMicroTabuleiro:
+    
+    # Testa se cada posição do tabuleiro foi iniciada como sendo vazia
+    def test_tabuleiro_vazio_pos00(self):
+        t = microTabuleiro()
+        assert t.configuracaoDoTabuleiro[0][0] == " "
+    def test_tabuleiro_vazio_pos01(self):
+        t = microTabuleiro()
+        assert t.configuracaoDoTabuleiro[0][1] == " "
+    def test_tabuleiro_vazio_pos02(self):
+        t = microTabuleiro()
+        assert t.configuracaoDoTabuleiro[0][2] == " "
+    def test_tabuleiro_vazio_pos10(self):
+        t = microTabuleiro()
+        assert t.configuracaoDoTabuleiro[1][0] == " "
+    def test_tabuleiro_vazio_pos11(self):
+        t = microTabuleiro()
+        assert t.configuracaoDoTabuleiro[1][1] == " "
+    def test_tabuleiro_vazio_pos12(self):
+        t = microTabuleiro()
+        assert t.configuracaoDoTabuleiro[1][2] == " "
+    def test_tabuleiro_vazio_pos20(self):
+        t = microTabuleiro()
+        assert t.configuracaoDoTabuleiro[2][0] == " "
+    def test_tabuleiro_vazio_pos21(self):
+        t = microTabuleiro()
+        assert t.configuracaoDoTabuleiro[2][1] == " "
+    def test_tabuleiro_vazio_pos22(self):
+        t = microTabuleiro()
+        assert t.configuracaoDoTabuleiro[2][2] == " "
+        
